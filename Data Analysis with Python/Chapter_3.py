54ba9871f1da412ce377ac77d76678f55b985ec7
Adil Hafeez <adilhafeez299@gmail.com>
2025-10-28 14:00:56 +0000

    Finished Chapter 3 - Tuple examples

---
+++ Data Analysis with Python/Chapter_3.py
@@ -0,0 +1,18 @@
+#Tuples 
+
+tup = (4, 5, 6 )
+tup
+print(tup)
+
+tup2 = 4, 5, 6
+print(tup2)
+
+#string conversion to tuple 
+tuple([4,0,2])
+print(tuple([4,0,2]))
+
+tup3 = tuple('Adil')
+print(tup[0])
+print(tup3)
+tup3[0] 
+print(tup3[0])  
